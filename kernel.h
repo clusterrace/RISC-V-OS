ba1827f9e139ff786508d07ac645a25557233232
Sus Verwimp <sus.verwimp@hotmail.com>
2025-01-13 22:19:35 +0100

    add exceptions + allocator

--- kernel.h
+++ kernel.h
@@ -1,6 +1,13 @@
 #pragma once
 
 struct sbiret {
-    long error;
-    long value;
+  long error;
+  long value;
 };
+
+#define PANIC(fmt, ...)                                                   \
+  do {                                                                    \
+    printf("%s - PANIC: %s:%d: " fmt "\n", __DATE__, __FILE__, __LINE__, ##__VA_ARGS__); \
+    while (1) {                                                           \
+    }                                                                     \
+  } while (0)
